85e60fd2a6c974c90b61e4fb6c0716e8f294d0b1
alesolano <asolanopf@gmail.com>
2021-11-02 11:52:56 +0100

    add flask app

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,9 @@ RUN apt-get update && apt-get install -y gcc ffmpeg libsm6 libxext6
 RUN pip install Cython
 RUN pip install -r requirements.txt
 
-#ENTRYPOINT jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
+COPY download_model.py .
+RUN python download_model.py
+
+COPY . .
+
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+from run import Run
+
+app = Flask(__name__)
+
+run = Run()
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    boxes = run.get_bboxes_from_video('./videos/video.mp4')
+    return jsonify(boxes)
+
+
+if __name__ == '__main__':
+    app.run(port=5001, host='0.0.0.0')
